3f2709d22c021e80fe05a1612c90339fc1539764
Razial <uck15@bk.ru>
2023-04-25 17:01:31 +0300

    Add orders view

--- orders/urls.py
+++ orders/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from orders.views import OrderCreateView, OrderSuccessView
+from orders.views import OrderCreateView, OrderSuccessView, OrderListView
 
 app_name = 'orders'
 
 urlpatterns = [
     path('create', OrderCreateView.as_view(), name='order_create'),
-    path('success/', OrderSuccessView.as_view(), name='success')
+    path('success/', OrderSuccessView.as_view(), name='success'),
+    path('', OrderListView.as_view(), name='orders_list'),
 
 ]
